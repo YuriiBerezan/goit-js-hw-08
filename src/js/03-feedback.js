4564186356adfe87e34be71b51a890273582b022
Yurii Berezan <67425022+YuriiBerezan@users.noreply.github.com>
2022-08-04 15:32:36 +0200

    fixed update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,44 +3,6 @@ import '../css/03-feedback.css'
 
 import throttle from 'lodash.throttle';
 
-// const STORAGE_KEY = 'feedback-msg';
-// const formData = {};
-// const refs = {
-//   form: document.querySelector('.feedback-form'),
-//   textarea: document.querySelector('.js-feedback-form textarea'),
-//   email: document.querySelector('.feedback-form input'),
-  
-// };
-// refs.form.addEventListener('submit', onFormSubmit);
-// refs.form.addEventListener('input', throttle(onTextareaInput, 200));
-// populateTextarea();
- 
-
-// refs.form.addEventListener('input', e => {
-//   formData[e.target.name] = e.target.value;
-//   console.log(formData);
-// });
-
-// function onFormSubmit(evt) {
-//   evt.preventDefault();
-//   evt.currentTarget.reset();
-//   localStorage.removeItem(STORAGE_KEY)
-  
-// }
-
-
-// function onTextareaInput(evt) {
-//   const message = evt.target.value;
-//   localStorage.setItem(STORAGE_KEY, message);
-//   console.log(message)
-// }
-
-// function populateTextarea() {
-//   const savedMessage = localStorage.getItem(STORAGE_KEY);
-//   if (savedMessage) {
-//     refs.textarea.value = savedMessage;
-//   }
-// }
 const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
@@ -61,16 +23,21 @@ function onSubmitForm(e) {
   }
 
 function dataFromLocalStorage() {
+    // data = '';
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  let email = document.querySelector('.feedback-form input');
+    let message = document.querySelector('.feedback-form textarea');
+  // data = '';
+  // if (data) {
+    if (email != ' ' & message != ' ') {
+      email.value = data.email;
+      message.value = data.message;
+    // }
+  }
+  
+  
     
-  let data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = document.querySelector('.feedback-form input');
-    const message = document.querySelector('.feedback-form textarea');
-    
-    if (data) {
-
-    email.value = data.email;
-    message.value = data.message;
-     }
+     
     
     // data = ' ';
 
